37ac5eab4bccd850c53faf46e4471540061a0089
juliuscaesar100usd <zhetibayevarsen@gmail.com>
2026-04-02 21:39:05 +0500

    first commit

---
+++ database.py
@@ -0,0 +1,19 @@
+from typing import Annotated
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+def get_db():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_db)]
+
---
+++ models.py
@@ -0,0 +1,44 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Enum, ForeignKey, engine
+from sqlalchemy.orm import relationship
+from database.py import Base
+from enum import Enum
+from datetime import datetime
+
+class Status(Enum):
+    incomplete = "incomplete"
+    complete = "complete"
+
+class Priority(Enum):
+    low = "low"
+    medium = "medium"
+    high = "high"
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True, nullable=False)
+    description = Column(String, nullable=True)
+    status = Column(Enum(Status), default=Status.incomplete)
+    priority = Column(Enum(Priority), default=Priority.medium)
+    deadline = Column(DateTime, nullable=True)
+    created_at = Column(DateTime, default=datetime.now(datetime.timezone.utc), nullable=False)
+    assigned_to = Column(Integer, ForeignKey("users.id"))
+    created_by = Column(Integer, ForeignKey("users.id"))
+
+    #owner = relationship("User", back_populates="tasks")
+    assignee = relationship("User", foreign_keys=[assigned_to])
+    creator = relationship("User", foreign_keys=[created_by])
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String)
+    is_admin = Column(Boolean, default=False)
+
+    #tasks = relationship("Task", back_populates="owner")
+    tasks_assigned = relationship("Task", foreign_keys="Task.assigned_to", back_populates="assignee")
+
+Base.metadata.create_all(engine)
