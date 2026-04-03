0d177056d486e83b6769ced61d5c381bb35bd377
juliuscaesar100usd <zhetibayevarsen@gmail.com>
2026-04-03 11:31:59 +0500

    auth

---
+++ auth.py
@@ -0,0 +1,17 @@
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+from datetime import datetime, timedelta, timezone
+from fastapi import HTTPException, status
+
+SECRET_KEY = "85e356d1297a41453b9c8e5f1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p7q8r9s0t1u2v3w4x5y6z7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+CryptContext = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return CryptContext.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return CryptContext.verify(plain_password, hashed_password)
+
