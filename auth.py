7bbc2411a8caec0de5dd88812e16b18d91fa008c
juliuscaesar100usd <zhetibayevarsen@gmail.com>
2026-04-03 11:48:05 +0500

    auth modified

--- auth.py
+++ auth.py
@@ -15,3 +15,5 @@ def hash_password(password: str) -> str:
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return CryptContext.verify(plain_password, hashed_password)
 
+
+
