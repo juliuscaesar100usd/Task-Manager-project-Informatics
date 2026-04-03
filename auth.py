2712d917ca38b1d028ebdf65fd8082a2bf9523c3
juliuscaesar100usd <zhetibayevarsen@gmail.com>
2026-04-03 21:20:07 +0500

    debugging

--- auth.py
+++ auth.py
@@ -7,13 +7,13 @@ SECRET_KEY = "85e356d1297a41453b9c8e5f1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p7q8r9s0t1u
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-CryptContext = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
-    return CryptContext.hash(password)
+    return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return CryptContext.verify(plain_password, hashed_password)
+    return pwd_context.verify(plain_password, hashed_password)
 
 def create_access_token(data: dict):
     to_encode = data.copy()
