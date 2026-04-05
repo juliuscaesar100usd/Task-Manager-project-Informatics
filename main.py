1a707f7cd04c61ca892ddf7848aa6c3db6de39f5
juliuscaesar100usd <zhetibayevarsen@gmail.com>
2026-04-05 19:18:53 +0500

    checkout main

--- main.py
+++ main.py
@@ -5,4 +5,7 @@ app= FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Server is running"}
+    return {"message": "App is running!"}
+
+#app.include_router(auth_router)
+#app.include_router(tasks_router)
