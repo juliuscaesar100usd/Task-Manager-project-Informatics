92f192881a2263ecc634dbafe8a73802d5a81c5d
juliuscaesar100usd <zhetibayevarsen@gmail.com>
2026-04-05 18:56:36 +0500

    import

---
+++ dependencies.py
@@ -0,0 +1,7 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from database import get_db
+from models import User
+from auth import decode_access_token
+
