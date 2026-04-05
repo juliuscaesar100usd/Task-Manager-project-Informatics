c0f5b0b21e53973b9bdb195ed78838a559182641
juliuscaesar100usd <zhetibayevarsen@gmail.com>
2026-04-05 19:12:32 +0500

    getter

--- dependencies.py
+++ dependencies.py
@@ -6,3 +6,10 @@ from models import User
 from auth import decode_access_token
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> User:
+    username = decode_access_token(token)
+    user = db.query(User).filter(User.username == username).first()
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
+    return user
