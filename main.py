e81163fbfa890fdd5e80604b32a5c340cc1e6420
juliuscaesar100usd <zhetibayevarsen@gmail.com>
2026-04-06 08:52:08 +0500

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from routers.auth import router as auth_router
+
+
 import models
 
 app= FastAPI()
@@ -7,5 +10,5 @@ app= FastAPI()
 def read_root():
     return {"message": "App is running!"}
 
-#app.include_router(auth_router)
+app.include_router(auth_router)
 #app.include_router(tasks_router)
