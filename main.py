4a9709819e509431708e27d83306393cd3d1500c
juliuscaesar100usd <zhetibayevarsen@gmail.com>
2026-04-07 23:04:06 +0500

    CORSMiddleware

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from routers.auth import auth_router
 from routers.tasks import tasks_router
-
+from fastapi.middleware.cors import CORSMiddleware
 
 import models
 
 app= FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
